aa0770e89bf2a06314d7fb9d392d84d3a557e1d1
namanh11611 <conan11611@yahoo.com.vn>
2017-01-05 23:09:10 +0700

    Add UIComponent in Edit (real) Product

--- app/code/Magestore/HelloMagento/Model/ResourceModel/Product/Grid/Collection.php
+++ app/code/Magestore/HelloMagento/Model/ResourceModel/Product/Grid/Collection.php
@@ -6,5 +6,18 @@ use Magento\Framework\View\Element\UiComponent\DataProvider\SearchResult;
 
 class Collection extends SearchResult
 {
+    protected function _construct()
+    {
+    }
 
+//    public function setItems(array $items = null)
+//    {
+//        if ($items) {
+//            foreach ($items as $item) {
+//                $this->addItem($item);
+//            }
+//            unset($this->totalCount);
+//        }
+//        return $this;
+//    }
 }
